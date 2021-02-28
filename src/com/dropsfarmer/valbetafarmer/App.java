c4ee29abad8d826d021f2b077ff9471c78f1c213
Miguel de los Dolores <migueldoloresgui@gmail.com>
2021-02-28 22:44:31 +0100

    Package refactoring from Valorant to twitch Drops

---
+++ src/com/dropsfarmer/valbetafarmer/App.java
@@ -0,0 +1,19 @@
+package com.dropsfarmer.valbetafarmer;
+
+import com.dropsfarmer.view.MainMenu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+        MainMenu m = new MainMenu();
+        m.setLocationRelativeTo(null);
+        m.setTitle("Twitch !Drops farmer");
+        m.setResizable(false);
+        m.setVisible(true);
+    }
+}
---
+++ src/com/dropsfarmer/var/Variables.java
@@ -0,0 +1,45 @@
+package com.dropsfarmer.var;
+
+import org.openqa.selenium.WebDriver;
+
+public class Variables {
+	private static String chrome_driver_route = "";
+	private static String login_user = "";
+	private static String login_pass = "";
+	private static WebDriver driver = null;
+	private static String game = "";
+	
+	
+	public static String getChrome_driver_route() {
+		return chrome_driver_route;
+	}
+	public static void setChrome_driver_route(String chrome_driver_route) {
+		Variables.chrome_driver_route = chrome_driver_route;
+	}
+	public static String getLogin_user() {
+		return login_user;
+	}
+	public static void setLogin_user(String login_user) {
+		Variables.login_user = login_user;
+	}
+	public static String getLogin_pass() {
+		return login_pass;
+	}
+	public static void setLogin_pass(String login_pass) {
+		Variables.login_pass = login_pass;
+	}
+	public static WebDriver getDriver() {
+		return driver;
+	}
+	public static void setDriver(WebDriver driver) {
+		Variables.driver = driver;
+	}
+	public static String getGame() {
+		return game;
+	}
+	public static void setGame(String game) {
+		Variables.game = game;
+	}
+	
+	
+}
